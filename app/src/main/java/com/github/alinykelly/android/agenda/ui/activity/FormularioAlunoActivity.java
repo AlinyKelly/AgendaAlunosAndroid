c73e2719d57ba38ccd4a5ea5fe9383d504189790
Aliny <alinykellyfs@gmail.com>
2022-08-28 22:02:21 -0300

    Initial commit

---
+++ app/src/main/java/com/github/alinykelly/android/agenda/ui/activity/FormularioAlunoActivity.java
@@ -0,0 +1,66 @@
+package com.github.alinykelly.android.agenda.ui.activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.alinykelly.android.agenda.R;
+import com.github.alinykelly.android.agenda.dao.AlunoDAO;
+import com.github.alinykelly.android.agenda.model.Aluno;
+
+public class FormularioAlunoActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Novo Aluno";
+    private EditText campoNome;
+    private EditText campoTelefone;
+    private EditText campoEmail;
+    private final AlunoDAO dao = new AlunoDAO();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_aluno);
+
+        setTitle(TITULO_APPBAR); //Altera a nome da barra de titulo do app
+
+        inicializacaoDosCampos();
+
+        configuraBotaoSalvar();
+    }
+
+    private void configuraBotaoSalvar() {
+        Button botaoSalvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Aluno alunoCriado = criaAluno();
+                salvar(alunoCriado);
+            }
+        });
+    }
+
+    private void inicializacaoDosCampos() {
+        campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+        campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+    }
+
+    private void salvar(Aluno aluno) {
+        dao.salva(aluno);
+        finish();
+    }
+
+    @NonNull
+    private Aluno criaAluno() {
+        String nome = campoNome.getText().toString();
+        String telefone = campoTelefone.getText().toString();
+        String email = campoEmail.getText().toString();
+
+        Aluno alunoCriado = new Aluno(nome, telefone, email);
+        return alunoCriado;
+    }
+}
